d69f618b1369c19e72b0a88794df54466b981d99
artem.ovcharenko <artem.ovcharenko@codilime.com>
2019-03-17 21:10:48 +0100

    05

---
+++ exercises/05-stack.js
@@ -0,0 +1,26 @@
+/**
+ * TODO: Write a function stack, which takes two arguments :
+ * first -  array of functions
+ * second - starting argument
+ * and execute them one by one.
+ * argument of each next function should be result of previous function or starting argument for the first function.
+ *
+ * function stack should returns result of the last function
+ * TODO: Note that array should be reverted because of stack tackes last function as first!
+ *
+ * usage example
+ * stack(
+ *  [
+   *  (arg) => { return arg + 1;},
+   *  (arg) => { return arg + 2;}
+   *  (arg) => { return arg + 3;}
+   *  (arg) => { return arg + 4;}
+ *  ],
+ *  0
+ * ) // 10
+ * */
+
+const stack = (array, startArg) => {};
+
+
+module.exports = stack;
---
+++ exercises/05-stack.test.js
@@ -0,0 +1,28 @@
+const stack = require('./05-stack');
+
+describe('missingNumber', () => {
+  it('should be defined', () => {
+    expect(stack).toBeDefined();
+  });
+
+  it('should be typeof function', () => {
+    expect(typeof stack).toBe('function');
+  });
+
+  it('should return a number', () => {
+    expect(typeof stack([(arg) => arg + 1 ], 0)).toBe('number');
+  });
+
+  it('should returns proper number easy case', () => {
+    expect(typeof stack([(arg) => arg + 1 ], 0)).toBe(1);
+  });
+
+  it('should returns proper number max case', () => {
+    expect(stack([
+      (arg) => { return arg + 1;},
+      (arg) => { return arg + 2;},
+      (arg) => { return arg + 3;},
+      (arg) => { return arg + 4;},
+    ], 0)).toEqual(10);
+  });
+});
